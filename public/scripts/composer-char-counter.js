1731d6f4b7ff3a3d56be5d54494683a2d629b801
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-18 18:02:20 +0000

    character counter script

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+// ensuring the code has loaded
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    let count = 140 - $(this).val().length;
+    let counter = $(this).parent().children('div.submit').children('.counter');
+    counter.text(count);
+    if (count < 0) {
+      counter.css('color', 'red');
+    } else if (count > 0) {
+      counter.css('color', "#545149");
+    }
+  });
+});
+
